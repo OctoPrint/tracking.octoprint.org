c50ec3ee4b2919da652ef6319efa93544c8a29d5
Gina Häußge <gina@octoprint.org>
2021-01-12 15:42:16 +0100

    Forget to adjust relative paths

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 set -e
 
 # nginx
-NGINX_LOCATION_SOURCE=scripts/nginx/tracking.location.conf
+NGINX_LOCATION_SOURCE=nginx/tracking.location.conf
 NGINX_LOCATION_TARGET=/etc/nginx/snippets/tracking.location.conf
-NGINX_LOG_SOURCE=scripts/nginx/logformat_tracking.conf
+NGINX_LOG_SOURCE=nginx/logformat_tracking.conf
 NGINX_LOG_TARGET=/etc/nginx/conf.d/logformat_tracking.conf
 
 if cmp -s "$NGINX_LOCATION_SOURCE" "$NGINX_LOCATION_TARGET" && cmp -s "$NGINX_LOG_SOURCE" "$NGINX_LOG_TARGET"; then
@@ -15,7 +15,7 @@ else
 fi
 
 # Logstash
-LOGSTASH_CONFIG_SOURCE=scripts/logstash/logstash-tracking-live.conf
+LOGSTASH_CONFIG_SOURCE=logstash/logstash-tracking-live.conf
 LOGSTASH_CONFIG_TARGET=/etc/logstash/conf.d/logstash-tracking-live.conf
 
 if cmp -s "$LOGSTASH_CONFIG_SOURCE" "$LOGSTASH_CONFIG_TARGET"; then
@@ -26,6 +26,6 @@ fi
 
 # Curator
 echo "Copying curator config"
-sudo cp -v scripts/curator/*.yml /etc/curator/
+sudo cp -v curator/*.yml /etc/curator/
 
 echo "All done!"
